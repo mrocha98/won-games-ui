db098fa67e6fc871f35272fdd72cb6742d302bbc
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-18 00:55:25 -0300

    feat: add size prop in Logo component

--- src/components/Logo/logo.spec.tsx
+++ src/components/Logo/logo.spec.tsx
@@ -6,7 +6,6 @@ import { Logo } from '.'
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
-
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
     })
@@ -14,9 +13,22 @@ describe('<Logo />', () => {
 
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
-
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo when size is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
