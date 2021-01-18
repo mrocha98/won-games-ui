2ac27e4e6792f8f20b0f92993de1b65b44e04c93
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-18 01:45:15 -0300

    feat: add hideOnMobile prop in Logo component

--- src/components/Logo/logo.spec.tsx
+++ src/components/Logo/logo.spec.tsx
@@ -31,4 +31,15 @@ describe('<Logo />', () => {
       width: '20rem'
     })
   })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
+  })
 })
