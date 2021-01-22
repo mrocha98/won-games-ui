fe4190a109dcbcfecf054d260c0e541b122d920d
Matheus Rocha <matheus_rocha98@outlook.com>
2021-01-22 11:26:06 -0300

    refactor: adds hover in Button component

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -49,6 +49,10 @@ export const Wrapper = styled.button<WrapperProps>`
     cursor: pointer;
     padding: ${theme.spacings.xxsmall};
 
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+    }
+
     ${!!size && wrapperModifiers[size](theme)}
     ${!!fullWidth && wrapperModifiers.fullWidth}
     ${!!hasIcon && wrapperModifiers.withIcon(theme)}
