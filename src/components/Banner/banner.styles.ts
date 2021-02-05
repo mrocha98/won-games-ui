31e63bc71421e21e4a4f360a677b0fa881a338e1
Matheus Rocha <matheus_rocha98@outlook.com>
2021-02-05 18:12:31 -0300

    feat: add Ribbon inside Banner

--- src/components/Banner/banner.styles.ts
+++ src/components/Banner/banner.styles.ts
@@ -1,11 +1,22 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+import * as RibbonStyles from 'components/Ribbon/ribbon.styles'
+
 export const Wrapper = styled.div`
   position: relative;
 
   ${media.greaterThan('medium')`
     box-shadow: 0 0.4rem 0.5rem 0 rgba(0, 0, 0, 0.2);
+  `}
+
+  ${media.lessThan('large')`
+    ${RibbonStyles.Wrapper} {
+      right: 0;
+      &::before {
+        display: none;
+      }
+    }
   `}
 `
 
