e7e75a9d85c90772146a6f0cf3db49e08e645084
Matheus Rocha <matheus_rocha98@outlook.com>
2021-02-16 17:32:19 -0300

    feat: create Radio component

---
+++ src/components/Radio/radio.component.tsx
@@ -0,0 +1,41 @@
+import { InputHTMLAttributes } from 'react'
+
+import * as S from './radio.styles'
+
+export type RadioProps = {
+  onCheck?: (value?: RadioValue) => void
+  label?: string
+  labelColor?: 'white' | 'black'
+  labelFor?: string
+  value?: RadioValue
+} & InputHTMLAttributes<HTMLInputElement>
+
+export const Radio = ({
+  label,
+  onCheck,
+  labelColor = 'white',
+  labelFor = '',
+  value,
+  ...props
+}: RadioProps) => {
+  const onChange = () => {
+    !!onCheck && onCheck(value)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="radio"
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
