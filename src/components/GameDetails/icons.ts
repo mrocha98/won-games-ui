7ffefc588fc873203425f7c8a53683be10f6459d
Matheus Rocha <matheus_rocha98@outlook.com>
2021-03-21 03:12:26 -0300

    feat: create GameDetails component

---
+++ src/components/GameDetails/icons.ts
@@ -0,0 +1,5 @@
+export {
+  Linux as LinuxIcon,
+  Apple as MacIcon,
+  Windows as WindowsIcon
+} from '@styled-icons/fa-brands'
---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,10 @@
+import { GameDetailsProps } from '.'
+
+export const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  releaseDate: '2020-11-21T23:00:00',
+  platforms: ['windows', 'mac', 'linux'],
+  publisher: 'Walkabout',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Narrative']
+}
